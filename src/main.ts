d1a84a628e63339b98de40bcef4f872134530985
HellLover <68247946+HellLover@users.noreply.github.com>
2024-11-14 12:55:01 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,20 @@ import "./__structures/Channel";
 import "./__structures/User";
 
 import { Client } from "./structures/Client";
+import express from "express";
 
 const client = new Client();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Handle HTTP requests
+app.get('/', (req, res) => res.send('Bot is running!'));
+
+// Start the HTTP server
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+})
 
 client.register(process.env.BOT_TOKEN as string);
 
